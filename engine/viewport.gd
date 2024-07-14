6d7b61f5eed208b9eb86c6d919713d98f52b77a7
Kyle <kylebkirkpatrick@live.com>
2024-07-15 04:03:51 +1000

    player, test scene, starlight plugin, noise fog

---
+++ engine/viewport.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var first_scene = global.scenes[0].instantiate()
+	get_node("sub_viewport_container/sub_viewport").add_child(first_scene)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- object/player.gd
+++ object/player.gd
@@ -1,5 +1,8 @@
-extends Node3D
+extends RigidBody3D
 
+@export var impulse_speed = 1
+@export var thrust_speed = 10
+@export var boost_speed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +11,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var input_direction = Input.get_vector("steer_left", "steer_right", "steer_up", "steer_down")
+	var direction = Vector3(input_direction.x, -input_direction.y, 0).normalized()
+	if direction:
+		apply_central_force(direction * impulse_speed)
