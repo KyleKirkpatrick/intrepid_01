c3aaa46b4eebac9471f54ed26a56d340c7887dad
Kyle <kylebkirkpatrick@live.com>
2024-07-15 16:05:51 +1000

    coordinate label

--- object/player.gd
+++ object/player.gd
@@ -4,6 +4,8 @@ extends RigidBody3D
 @export var thrust_speed = 10
 @export var boost_speed = 100
 
+@onready var coordinate_label = $camera_2d/container/coordinate_label
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,3 +17,9 @@ func _process(delta):
 	var direction = Vector3(input_direction.x, -input_direction.y, 0).normalized()
 	if direction:
 		apply_central_force(direction * impulse_speed)
+	update_HUD()
+
+func update_HUD():
+	var coordinates = global_position
+	coordinate_label.clear()
+	coordinate_label.append_text("X: " + str(roundf(coordinates.x)) + "\nY: " + str(roundf(coordinates.y)))
